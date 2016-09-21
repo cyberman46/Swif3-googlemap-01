867029037ddcb8eb1a74e0c9509686da6a3d063b
cyberman46 <akekapon27@gmail.com>
2016-09-21 13:50:40 +0700

    add google map with marker

--- iosMap/ViewController.swift
+++ iosMap/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
-
+import GoogleMaps
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //loadView()
+        
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 13.727212, longitude: 100.540639, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.mapType = kGMSTypeHybrid
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 13.727212, longitude: 100.540639)
+        marker.title = "Kratostracking"
+        marker.snippet = "อาคารอื้อจือเหลียง"
+        marker.map = mapView
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
