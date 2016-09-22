13151c9b6336682088f9bb145a9b47a8b64b0a1d
cyberman46 <akekapon27@gmail.com>
2016-09-22 11:11:45 +0700

    add NavBar

--- iosMap/ViewController.swift
+++ iosMap/ViewController.swift
@@ -10,23 +10,37 @@ import UIKit
 import GoogleMaps
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container:UIView!
+    @IBOutlet weak var mButton:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //loadView()
         
         
-        let camera = GMSCameraPosition.camera(withLatitude: 13.727212, longitude: 100.540639, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 13.727212, longitude: 100.540639, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.mapType = kGMSTypeHybrid
         mapView.isMyLocationEnabled = true
+        mapView.settings.compassButton = true
+        mapView.settings.indoorPicker = true
+        mapView.settings.myLocationButton = true
+        mapView.mapType = kGMSTypeHybrid
+        
         view = mapView
+        
+        self.container = mapView
         
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 13.727212, longitude: 100.540639)
         marker.title = "Kratostracking"
         marker.snippet = "อาคารอื้อจือเหลียง"
         marker.map = mapView
+    
+        
+    }
+    
+    @IBAction func click(){
         
         
     }
